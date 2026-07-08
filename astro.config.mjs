16f7962287963ea877a1d517d8b21d8abd98ee0c
Anthony James <anthonyjames@rigrescue.net>
2026-07-08 14:34:09 -0400

    Enable sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,16 @@
 import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare(),
-
-  vite: {
-    plugins: [tailwindcss()]
-  }
+                            site: 'https://rigrescue.net',
+                            integrations: [
+                              // Add sitemap here
+                            ],
+                            vite: {
+                              plugins: [tailwindcss()]
+                            }
 });
