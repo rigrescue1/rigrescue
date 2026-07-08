c9d13fca061cbb560432face1b81fa01630de144
Anthony James <anthonyjames@rigrescue.net>
2026-07-08 14:38:52 -0400

    Added sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import cloudflare from '@astrojs/cloudflare';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare(),
                             site: 'https://rigrescue.net',
-                            integrations: [
-                              // Add sitemap here
-                            ],
+                            integrations: [sitemap()],
                             vite: {
                               plugins: [tailwindcss()]
                             }
